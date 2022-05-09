5082b586609e8cff70d6807041cf07816c53729c
Ricardo Meneses <ricardo.meneses@unosquare.com>
2022-05-09 15:19:42 -0500

    First commit

---
+++ src/main/java/com/unosquare/coe/lexington/LexingtonApplication.java
@@ -0,0 +1,13 @@
+package com.unosquare.coe.lexington;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexingtonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexingtonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unosquare/coe/lexington/LexingtonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unosquare.coe.lexington;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexingtonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
